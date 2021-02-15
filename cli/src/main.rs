7fc5403b076c21692ceb0eed1a6c4e10946db37d
Alexander Meißner <AlexanderMeissner@gmx.net>
2021-02-15 15:07:37 +0100

    v0.2.5 (#141)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -303,7 +303,7 @@ impl AnalysisResult {
 
 fn main() {
     let matches = App::new("Solana RBPF CLI")
-        .version("0.2.4")
+        .version("0.2.5")
         .author("Solana Maintainers <maintainers@solana.foundation>")
         .about("CLI to test and analyze eBPF programs")
         .arg(
