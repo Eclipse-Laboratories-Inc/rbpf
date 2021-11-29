c14764850f0b83b58aa013248eaf6d65836c1218
Alexander Meißner <AlexanderMeissner@gmx.net>
2021-11-29 19:25:28 +0100

    Bump to v0.2.16 (#234)
    Includes #227 to #233.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -38,7 +38,7 @@ impl SyscallObject<UserError> for MockSyscall {
 
 fn main() {
     let matches = App::new("Solana RBPF CLI")
-        .version("0.2.15")
+        .version("0.2.16")
         .author("Solana Maintainers <maintainers@solana.foundation>")
         .about("CLI to test and analyze eBPF programs")
         .arg(
